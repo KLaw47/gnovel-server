8ab676ad3057f83641e3728a80cb64b801773423
Keaton Law <keaton.c.law@gmail.com>
2023-02-27 20:07:31 -0600

    completed average rating

--- gnovelapi/views/comic.py
+++ gnovelapi/views/comic.py
@@ -8,7 +8,6 @@ from gnovelapi.models import Comic, Review
 class ComicView(ViewSet):
 
     def retrieve(self, request, pk):
-
         try:
             comic = Comic.objects.get(pk=pk)
             reviews = Review.objects.filter(comic=comic.id)
@@ -19,20 +18,26 @@ class ComicView(ViewSet):
         except Comic.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
-
     def list(self, request):
-
         comics = Comic.objects.all()
-
         serializer = ComicSerializer(comics, many=True)
         return Response(serializer.data)
-
 class ComicSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Comic
-        fields = ('id', 'title', 'description', 'thumbnail')
+        fields = ('id', 'title', 'description', 'thumbnail', 'average_rating')
         depth = 1
+
+    def get_average_rating(self, comic):
+        reviews = Review.objects.filter(comic=comic)
+        if reviews:
+            ratings = [review.rating for review in reviews]
+            return sum(ratings) / len(ratings)
+        else:
+            return 0
+
 
 class ReviewSerializer(serializers.ModelSerializer):
 
